bf19f7eaca63a19dedbeb1476660fd91df46ba46
eman <tenhitcombo@gmail.com>
2019-08-11 20:23:40 -0400

    image fill control test

---
+++ Assets/Scripts/ImageFillTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFillTest : MonoBehaviour
+{
+    private Image imageInstance;
+    public int sign = 1;
+    public bool buttonPressed = false;
+
+    private float percentage;
+    private float currentAmount;
+    private float speed = 30;
+
+    void Start()
+    {
+        imageInstance = GetComponent<Image>();
+
+        if (imageInstance != null)
+        {
+            percentage = imageInstance.fillAmount * 100;
+        }
+    }
+
+    void Update()
+    {
+        ButtonDown();
+        ButtonUp();
+
+        if (buttonPressed)
+        {
+            percentage += sign;
+            print(percentage);
+
+            if (percentage >= 100 || percentage <= 0)
+            {
+                sign *= -1;
+                percentage = ((percentage <= 0) ? 0 : 100);
+            }
+        }
+        imageInstance.fillAmount = percentage / 100;
+    }
+
+    void ButtonDown()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            buttonPressed = true;
+        }
+    }
+
+    void ButtonUp()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            buttonPressed = false;
+        }
+    }
+}
--- Assets/Scripts/TextTest.cs
+++ Assets/Scripts/TextTest.cs
@@ -7,13 +7,14 @@ public class TextTest : MonoBehaviour
 {
     public string message;
     private Text textInstace;
+    
 
     // Start is called before the first frame update
     void Start() {
         //? get the component of the text field on the game object this script is attached to
         //* set the value of the textInstance to equal the GetComponent method call and I pass in the text as a type for it to search for.
         textInstace = GetComponent<Text>(); 
-
+        
     }
 
     // Update is called once per frame
