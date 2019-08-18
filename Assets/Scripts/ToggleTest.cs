bd3cc0b4f8cc46d6d29a417094689d2cb94de748
eman <tenhitcombo@gmail.com>
2019-08-18 14:07:50 -0400

    toggle script & added sfx for SF2 menu proj

---
+++ Assets/Scripts/ToggleTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class ToggleTest : MonoBehaviour
+{
+    ToggleGroup toggleGroupinstance;
+
+    public Toggle currentSelection{
+        get {return toggleGroupinstance.ActiveToggles().FirstOrDefault();}
+    }
+  
+    void Start()
+    {
+        toggleGroupinstance = GetComponent<ToggleGroup>();    
+        Debug.Log("Linq: First selected " + currentSelection.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
