b0e658dde31ed4416a528a5c71a6e245d100e16e
eman <tenhitcombo@gmail.com>
2019-08-18 22:52:43 -0400

    mostly images & event system test

---
+++ Assets/Scripts/sfxTriggerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class sfxTriggerTest : MonoBehaviour
+{
+    private Button buttons;
+    private EventSystem eventSystem;
+    private Button currentButton;
+    public AudioClip highlightSound;
+    private AudioSource source {get {return GetComponent<AudioSource>(); }}
+
+    private void Awake() {
+        gameObject.AddComponent<AudioSource>();
+        eventSystem = FindObjectOfType<EventSystem>();
+    }
+    
+    void Start()
+    {
+        source.clip = highlightSound;
+        source.playOnAwake = false;
+    }
+}
